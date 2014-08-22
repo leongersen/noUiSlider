e6ab28d7da65db798f1359028fc049a2d0b51495
Leon Gersen <leongersen@gmail.com>
2014-08-22 20:10:57 +0200

    Added wNumb to the compile script

--- compile.bat
+++ compile.bat
@@ -1,7 +1,8 @@
 @echo off
 
 :: Set all paths as variables
-set "wnumb=wnumb\wnumb.js"
+set "wnumb=wnumb\wNumb.js"
+set "wnumbMin=wnumb\wNumb.min.js"
 set "link=libLink\jquery.liblink.js"
 
 set "jquery=compiler\jquery-1.9.js"
@@ -12,25 +13,30 @@ set "options=src\module.options.js"
 set "source=src\module.base.js"
 set "pips=src\optional.pips.js"
 
-set "minresult=jquery.nouislider.min.js"
-set "fullresult=jquery.nouislider.full.min.js"
+set "nouislidermin=jquery.nouislider.min.js"
+set "nouisliderfull=jquery.nouislider.full.min.js"
+
+set "compiler=compiler\compiler.jar"
+set "level=--compilation_level SIMPLE_OPTIMIZATIONS"
+set "verbose=--warning_level VERBOSE"
 
 :: Remove the existing file so we can be sure a new one is generated.
-if exist %minresult% (
-	del %minresult%
-	echo "Removed %minresult%."
+if exist %nouislidermin% (
+	del %nouislidermin%
+	echo "Removed %nouislidermin%."
 )
-if exist %fullresult% (
-	del %fullresult%
-	echo "Removed %fullresult%,"
+if exist %nouisliderfull% (
+	del %nouisliderfull%
+	echo "Removed %nouisliderfull%,"
 )
 
 echo "Ready."
 
 PAUSE
 
-java -jar compiler\compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --externs %jquery% --externs %zepto% --warning_level VERBOSE --js %range% --js %options% --js %source% --js_output_file %minresult%
-java -jar compiler\compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --externs %jquery% --externs %zepto% --warning_level VERBOSE --js %wnumb% --js %link% --js %range% --js %options% --js %source% --js %pips% --js_output_file %fullresult%
+java -jar %compiler% %level% %verbose% --js %wnumb% --js_output_file %wnumbMin%
+java -jar %compiler% %level% %verbose% --externs %jquery% --externs %zepto% --js %range% --js %options% --js %source% --js_output_file %nouislidermin%
+java -jar %compiler% %level% %verbose% --externs %jquery% --externs %zepto% --js %wnumb% --js %link% --js %range% --js %options% --js %source% --js %pips% --js_output_file %nouisliderfull%
 
 echo "Done."
 PAUSE
