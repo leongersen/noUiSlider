993c8051c8ced1943789f0d62add54a7d2779f76
Leon Gersen <leongersen@gmail.com>
2015-06-27 14:08:11 +0200

    Language cleanup

--- documentation/examples.php
+++ documentation/examples.php
@@ -269,11 +269,11 @@
 
 	<div class="view">
 
-		<p>To keep the plugin small, features like keyboard interaction haven't been included. However, adding custom features to the input fields linked to the plugin is easy. noUiSlider provides <strong>API's</strong> to help you. In this example, pressing the keyboard arrow keys will increase/decrease the slider by one step.</p>
+		<p>To keep the library small, features like keyboard interaction haven't been included. However, adding features to input fields linked to a slider is easy. noUiSlider provides API's to help you. In this example, pressing the keyboard arrow keys will increase/decrease the slider by one step.</p>
 
 		<p>This example uses the <code>'step'</code> API to determine by how much the slider should be changed. You don't need this function if your slider is linear. In that case, increase/decrease the value with the ammount of your <code>step</code>.</p>
 
-		<p>We'll listen to keydown on the <code>$('#input-with-keypress')</code> element, and pass the event to a function so we can read the code that identifies the key.</p>
+		<p>We'll listen to keydown on the <code>'#input-with-keypress'</code> element, and pass the event to a function so we can read the code that identifies the key.</p>
 
 		<p>Note that the slider value will be a <code>string</code>, so we'll need to parse it to an integer.</p>
 
