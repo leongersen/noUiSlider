86bfed84056dec0725d4ce598756e24e3e7ac34b
Leon Gersen <leongersen@gmail.com>
2015-06-27 15:10:05 +0200

    Left debug behind

--- documentation/slider-values/range-link.js
+++ documentation/slider-values/range-link.js
@@ -1,5 +1,5 @@
 var rangeSliderValueElement = document.getElementById('slider-range-value');
 
-rangeSlider.noUiSlider.on('update', function( values, handle, unencoded ) {
-	rangeSliderValueElement.innerHTML = values[handle] + ', ' + unencoded;
+rangeSlider.noUiSlider.on('update', function( values, handle ) {
+	rangeSliderValueElement.innerHTML = values[handle];
 });
