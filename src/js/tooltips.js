0d759ec28b6766f1f9f60bb03e690364c52ef270
Leon Gersen <leongersen@gmail.com>
2015-10-09 15:41:25 +0200

    Cleanup tooltip implementation

--- src/js/tooltips.js
+++ src/js/tooltips.js
@@ -1,18 +1,19 @@
+function defaultFormatTooltipValue(formattedValue) {
+	return formattedValue;
+}
+
+function addTooltip( handle ) {
+	var element = document.createElement('div');
+	element.className = cssClasses[18];
+	return handle.firstChild.appendChild(element);
+}
+
 function tooltips( tooltipsOptions ) {
-  var positionClasses = [cssClasses[19], cssClasses[20]];
-  var formatTooltipValue = tooltipsOptions.format ? tooltipsOptions.format : defaultFormatTooltipValue;
 
-  var tooltips = scope_Handles.map(function addTooltip(handle, handleId) {
-    var element = document.createElement('div');
-    element.className = cssClasses[18];
-    return handle.firstChild.appendChild(element);
-  });
+	var formatTooltipValue = tooltipsOptions.format ? tooltipsOptions.format : defaultFormatTooltipValue,
+		tips = scope_Handles.map(addTooltip);
 
-  bindEvent('update', function updateTooltip(formattedValues, handleId, rawValues) {
-    tooltips[handleId].innerHTML = formatTooltipValue(formattedValues[handleId], rawValues[handleId]);
-  });
-
-  function defaultFormatTooltipValue(formattedValue, rawValue) {
-    return formattedValue;
-  }
+	bindEvent('update', function(formattedValues, handleId, rawValues) {
+		tips[handleId].innerHTML = formatTooltipValue(formattedValues[handleId], rawValues[handleId]);
+	});
 }
