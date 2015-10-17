a35df640a54f3460343c36fb60c9856ebdaaf563
Leon Gersen <leongersen@gmail.com>
2015-10-17 17:18:48 +0200

    Format

--- src/js/tooltips.js
+++ src/js/tooltips.js
@@ -1,19 +1,21 @@
-function defaultFormatTooltipValue ( formattedValue ) {
-	return formattedValue;
-}
 
-function addTooltip ( handle ) {
-	var element = document.createElement('div');
-	element.className = cssClasses[18];
-	return handle.firstChild.appendChild(element);
-}
+	function defaultFormatTooltipValue ( formattedValue ) {
+		return formattedValue;
+	}
 
-function tooltips ( tooltipsOptions ) {
+	function addTooltip ( handle ) {
+		var element = document.createElement('div');
+		element.className = cssClasses[18];
+		return handle.firstChild.appendChild(element);
+	}
 
-	var formatTooltipValue = tooltipsOptions.format ? tooltipsOptions.format : defaultFormatTooltipValue,
-		tips = scope_Handles.map(addTooltip);
+	// The tooltips option is a shorthand for using the 'update' event.
+	function tooltips ( tooltipsOptions ) {
 
-	bindEvent('update', function(formattedValues, handleId, rawValues) {
-		tips[handleId].innerHTML = formatTooltipValue(formattedValues[handleId], rawValues[handleId]);
-	});
-}
+		var formatTooltipValue = tooltipsOptions.format ? tooltipsOptions.format : defaultFormatTooltipValue,
+			tips = scope_Handles.map(addTooltip);
+
+		bindEvent('update', function(formattedValues, handleId, rawValues) {
+			tips[handleId].innerHTML = formatTooltipValue(formattedValues[handleId], rawValues[handleId]);
+		});
+	}
