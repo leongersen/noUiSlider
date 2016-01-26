476cee2d71247fb5c7098cf4594c34ab6cd7e0d2
Mikhail Pospelov <mixan946@gmail.com>
2016-01-26 19:15:05 +0300

    fix updateOptions ignoring current direction

---
+++ tests/slider_rtl_update_options.js
@@ -0,0 +1,32 @@
+
+  QUnit.test( "RTL slider with updateOptions", function( assert ){
+
+    Q.innerHTML = '<div class="slider"></div>';
+
+    var slider = Q.querySelector('.slider');
+
+    noUiSlider.create(slider, {
+      range: {
+        'min': 0,
+        'max': 50
+      },
+      start: [10, 40],
+      behaviour: 'tap',
+      connect: true,
+      direction : 'rtl'
+    });
+
+    slider.noUiSlider.updateOptions({ range: { max: 100 ,min: 0 } })
+
+    assert.deepEqual(slider.noUiSlider.get(), ["10.00", "40.00"], "Proper start handling in RTL.");
+
+    slider.noUiSlider.set([20,30]);
+    assert.deepEqual(slider.noUiSlider.get(), ["20.00", "30.00"]);
+
+    slider.noUiSlider.set([40,45]);
+    assert.deepEqual(slider.noUiSlider.get(), ["40.00", "45.00"], "RTL slider overstepped properly.");
+
+    slider.noUiSlider.set([30,35]);
+    assert.deepEqual(slider.noUiSlider.get(), ["30.00", "35.00"], "RTL slider understepped properly.");
+
+  });
