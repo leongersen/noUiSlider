0048e9c72016a3b79fdd30bf23620a20ebe95efc
Leon Gersen <leongersen@gmail.com>
2016-09-27 11:09:55 +0200

    streamline index

--- documentation/index.php
+++ documentation/index.php
@@ -3,11 +3,12 @@
 	$description = "noUiSlider is a free and lightweight JavaScript range slider with full touch support (iOS, Android, Windows 8). Great for responsive designs, and no dependencies!";
 ?>
 
-<h3>noUiSlider: lightweight JavaScript range slider</h3>
-
 <section>
 
 	<div class="view">
+
+		<h3>noUiSlider: lightweight JavaScript range slider</h3>
+		<br>
 
 		<p>noUiSlider is a range slider <strong>without bloat</strong>. It offers a ton off <strong>features</strong>, and it is as small, <strong>lightweight</strong> and minimal as possible, which is great for mobile use on the many supported <strong>devices</strong>, including iPhone, iPad, Android devices &amp; Windows (Phone) 8 desktops, tablets and all-in-ones. It works on desktops too, of course!</p>
 
@@ -15,12 +16,14 @@
 
 			<div id="range"></div>
 
-			<div style="text-align: center;">
-				<div id="range-value-3"></div>
-				<div id="range-diff-2" style="color: blue"></div>
-				<div id="range-value-2"></div>
-				<div id="range-diff-1" style="color: blue"></div>
-				<div id="range-value-1"></div>
+			<div style="text-align: center; font-size: 12px;">
+				<span id="range-value-4"></span> -
+				<span id="range-diff-3" style="color: blue"></span> -
+				<span id="range-value-3"></span> -
+				<span id="range-diff-2" style="color: blue"></span> -
+				<span id="range-value-2"></span> -
+				<span id="range-diff-1" style="color: blue"></span> -
+				<span id="range-value-1"></span>
 			</div>
 
 			<?php run('showcase'); ?>
@@ -52,29 +55,22 @@
 			<li>Touch support for iOS, Android &amp; Windows (phone)</li>
 			<li>Draggable range</li>
 			<li><strong>No jQuery, jQueryUI or other dependencies</strong></li>
-			<li>Tested in IE9 - IE11, Chrome, Opera, Firefox &amp; Safari</li>
+			<li>Tested in IE9 - IE11, Edge, Chrome, Opera, Firefox &amp; Safari</li>
 		</ul>
 
 		<p>noUiSlider works with pretty much any device, mouse, touchscreen or both, and it'll work beautifully in <strong>responsive designs</strong>. Have you tried this documentation on your phone?</p>
 	</div>
-</section>
 
+	<div class="side">
 
-<?php sect('getting-started'); ?>
-<h2>Getting started</h2>
+		<?php sect('getting-started'); ?>
+		<h2>Getting started</h2>
 
-<section>
-
-	<div class="view">
-
-		<p>noUiSlider 8 has no dependencies, so you don't need jQuery, jQuery UI, Zepto, etc. to use it.</p>
+		<p>noUiSlider has no dependencies, so you don't need jQuery, jQuery UI, Zepto, etc. to use it.</p>
 
 		<p>Putting all your scripts in the page <code>&lt;head&gt;</code> will slow down your site. If you'd like to know why, consider reading <a href="http://developer.yahoo.com/performance/rules.html#js_bottom">this article by Yahoo!</a>. Put the slider CSS in your <code>&lt;head&gt;</code>, and the script at the end of the <code>&lt;body&gt;</code>.</p>
 
 		<p>To create a slider, call <code>noUiSlider.create()</code> with an element and your options.</p>
-	</div>
-
-	<div class="side">
 
 <pre class="language-markup"><code>&lt;!-- In &lt;head&gt; --&gt;
 &lt;link href="nouislider.min.css" rel="stylesheet"&gt;
@@ -82,17 +78,11 @@
 &lt;!-- In &lt;body&gt; --&gt;
 &lt;script src="nouislider.min.js"&gt;&lt;/script&gt;</code></pre>
 
-	</div>
-</section>
 
+		<?php sect('browser-support'); ?>
+		<h2>Browser support</h2>
 
-<?php sect('browser-support'); ?>
-<h2>Browser support</h2>
+		<p>noUiSlider supports <strong>IE9 and up</strong>, and the latest versions of <strong>Edge</strong>, <strong>Chrome</strong>, <strong>Safari</strong>, <strong>Firefox</strong> and <strong>Opera</strong>.</p>
 
-<section>
-
-	<div class="view">
-
-		<p>noUiSlider supports <strong>IE9 and up</strong>, and the latest versions of the <em>'evergreen'</em> browsers: <strong>Chrome</strong>, <strong>Safari</strong>, <strong>Firefox</strong> and <strong>Opera</strong>.</p>
 	</div>
 </section>
--- documentation/index/showcase.js
+++ documentation/index/showcase.js
@@ -1,10 +1,10 @@
 var range = document.getElementById('range');
 
-range.style.height = '300px';
+range.style.height = '400px';
 range.style.margin = '0 auto 30px';
 
 noUiSlider.create(range, {
-	start: [ 1450, 2050, 2350 ], // Handle count and start position
+	start: [ 1450, 2050, 2350, 3000 ], // 4 handles, starting at...
 	margin: 300, // Handles must be at least 300 apart
 	limit: 600, // ... but no more than 600
 	connect: true, // Display a colored bar between the handles
@@ -12,12 +12,13 @@ noUiSlider.create(range, {
 	orientation: 'vertical', // Orient the slider vertically
 	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
 	step: 150,
+	tooltips: true,
 	format: wNumb({
 		decimals: 0
 	}),
 	range: {
 		'min': 1300,
-		'max': 2800
+		'max': 3250
 	},
 	pips: { // Show a scale with the slider
 		mode: 'steps',
