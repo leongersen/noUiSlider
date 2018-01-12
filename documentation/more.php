3000248f604cc21e4f04f6bdfec9f980124d6d9e
Léon Gersen <leongersen@gmail.com>
2018-01-12 19:42:35 +0000

    Documentation on update

--- documentation/more.php
+++ documentation/more.php
@@ -72,11 +72,15 @@
 
 	<div class="view">
 
-		<p>noUiSlider has an update method that can change the <code>'margin'</code>, <code>'limit'</code>, <code>'step'</code>, <code>'range'</code>, <code>'animate'</code> and <code>'snap'</code> options. All other options require changes to the slider's HTML or event bindings.</p>
+		<p>noUiSlider has an update method that can change the <code>'margin'</code>, <code>'limit'</code>, <code>'step'</code>, <code>'range'</code>, <code>'animate'</code> and <code>'snap'</code> options.</p>
 
-		<p>To update any other option, destroy the slider using <code>slider.noUiSlider.destroy()</code> and create a new one. Note that events are <strong>not</strong> unbound when destroying a slider.</p>
+		<p>All other options require changes to the slider's HTML or event bindings.</p>
+
+		<p>To update any other option, destroy the slider using <code>slider.noUiSlider.destroy()</code> and create a new one. Events are unbound when destroying a slider.</p>
 
 		<p>The update method can be called as <code>slider.noUiSlider.updateOptions(newOptions, [fireSetEvent])</code>.</p>
+
+		<p>Options that can not be updated will be ignored without errors.</p>
 
 		<p>The <code>'update'</code> event fires after updating the slider.</p>
 
