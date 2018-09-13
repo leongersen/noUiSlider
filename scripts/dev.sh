53b76e6a1e511a26abda0ddd27c3e07af107fce3
Leon Gersen <leongersen@gmail.com>
2018-09-13 07:16:13 +0200

    V12: Cleanup dev scripts

--- scripts/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit when error
-
-# & wait added so that if any of the command fails
-# you can still CTRL+C the whole thing (no phantom process)
-npm run watch:js & npm run serve-tests & wait
