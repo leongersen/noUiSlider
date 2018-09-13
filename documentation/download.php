27289a74a4b72ae2aef0f0f954cc08cf44b9cada
Leon Gersen <leongersen@gmail.com>
2018-09-13 13:12:21 +0200

    V12 Revise download page

--- documentation/download.php
+++ documentation/download.php
@@ -1,24 +1,32 @@
 <?php
-	$title = "noUiSlider - Download noUiSlider for free!";
-	$description = "Download the latest version of noUiSlider safely from gitHub. For free!";
+$title = "noUiSlider - Download noUiSlider for free!";
+$description = "Download the latest version of noUiSlider safely from gitHub. For free!";
 ?>
 
 <h1>Download noUiSlider</h1>
 
 <section>
 
-	<div class="view">
+    <div class="view">
 
-		<p>noUiSlider is open source, and you can use it <strong>for free</strong> in any personal or commercial product. No attribution required. Both the uncompressed and compressed version of noUiSlider are available in a <code>.zip</code> release, which is hosted by Github and available over <code>https</code>.</p>
+        <p>noUiSlider is open source (MIT), and you can use it <strong>for free</strong> in any personal or commercial
+            product. No attribution required.</p>
 
-		<a class="button" href="https://github.com/leongersen/noUiSlider/archive/<?php echo $version; ?>.zip" data-category="convert" data-action="download">Download noUiSlider from Github</a>
-
-	</div>
+        <a href="/nouislider/tests/slider.html">Run the unit tests!</a>
+    </div>
 </section>
 
+<section>
+    <h3>Install noUiSlider using a package manager:</h3>
+
+    <pre><code class="language-console">npm <span class="token function">install</span> <span class="token keyword">nouislider</span></code></pre>
+    <pre><code class="language-console">yarn <span class="token function">add</span> <span class="token keyword">nouislider</span></code></pre>
+
+</section>
 
 <section>
-	<a href="/nouislider/contact/">Help and contact</a>
-	<br>
-	<a href="/nouislider/tests/slider.html">Run the unit tests!</a>
+    <h3>Download noUiSlider</h3>
+
+    <a class="button" href="https://github.com/leongersen/noUiSlider/releases" data-category="convert" data-action="download">Download noUiSlider from Github</a>
+
 </section>
