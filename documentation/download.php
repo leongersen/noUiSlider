27e91bbd74c6bd31b20fd5b577745d0d58bc0cb2
Leon Gersen <leongersen@gmail.com>
2018-09-13 11:49:58 +0200

    V12 remove need to package distribution files

--- documentation/download.php
+++ documentation/download.php
@@ -11,7 +11,7 @@
 
 		<p>noUiSlider is open source, and you can use it <strong>for free</strong> in any personal or commercial product. No attribution required. Both the uncompressed and compressed version of noUiSlider are available in a <code>.zip</code> release, which is hosted by Github and available over <code>https</code>.</p>
 
-		<a class="button" href="https://github.com/leongersen/noUiSlider/releases/download/<?php echo $version; ?>/noUiSlider.<?php echo $version; ?>.zip" data-category="convert" data-action="download">Download noUiSlider from Github</a>
+		<a class="button" href="https://github.com/leongersen/noUiSlider/archive/<?php echo $version; ?>.zip" data-category="convert" data-action="download">Download noUiSlider from Github</a>
 
 	</div>
 </section>
@@ -21,15 +21,4 @@
 	<a href="/nouislider/contact/">Help and contact</a>
 	<br>
 	Run the unit tests: <a href="/nouislider/tests/range.html">range</a> and <a href="/nouislider/tests/slider.html">slider</a>.
-</section>
-
-
-<h2>License</h2>
-
-<section>
-
-	<div class="view">
-
-		<p>You can use noUiSlider for free, under the terms of the <a data-category="outlink" data-action="license" href="http://www.wtfpl.net/about/">WTFPL</a>, which allows everything and has no additional restrictions.</p>
-	</div>
 </section>
