9921f2679b898afbd94708b02ddce6d8684dcc4c
l.gersen@visymo.com <leongersen@gmail.com>
2019-03-13 13:40:44 +0100

    noUiSlider 13.1.2

--- distribute/nouislider.js
+++ distribute/nouislider.js
@@ -1,4 +1,4 @@
-/*! nouislider - 13.1.1 - 2/14/2019 */
+/*! nouislider - 13.1.2 - 3/13/2019 */
 (function(factory) {
     if (typeof define === "function" && define.amd) {
         // AMD. Register as an anonymous module.
@@ -13,7 +13,7 @@
 })(function() {
     "use strict";
 
-    var VERSION = "13.1.1";
+    var VERSION = "13.1.2";
 
     //region Helper Methods
 
@@ -1104,6 +1104,10 @@
             return addNodeTo(handle.firstChild, options.cssClasses.tooltip);
         }
 
+        function isSliderDisabled() {
+            return scope_Target.hasAttribute("disabled");
+        }
+
         // Disable the slider dragging if any handle is disabled
         function isHandleDisabled(handleNumber) {
             var handleOrigin = scope_Handles[handleNumber];
@@ -1443,7 +1447,7 @@
 
                 // doNotReject is passed by all end events to make sure released touches
                 // are not rejected, leaving the slider "stuck" to the cursor;
-                if (scope_Target.hasAttribute("disabled") && !data.doNotReject) {
+                if (isSliderDisabled() && !data.doNotReject) {
                     return false;
                 }
 
@@ -1783,7 +1787,7 @@
         // Handles keydown on focused handles
         // Don't move the document when pressing arrow keys on focused handles
         function eventKeydown(event, handleNumber) {
-            if (isHandleDisabled(handleNumber)) {
+            if (isSliderDisabled() || isHandleDisabled(handleNumber)) {
                 return false;
             }
 
