598d683af7acee016389ea36b4ed2b5f099919f8
Macro-Jackson <frank@comphare.com>
2020-05-08 16:29:51 +0200

    Added test non-linear sliders
    Added test for margins, limits and padding of non-linear sliders

--- tests/slider_limit.js
+++ tests/slider_limit.js
@@ -34,3 +34,44 @@ QUnit.test("Limit option", function (assert) {
     slider.noUiSlider.set([120, 240]);
     assert.deepEqual(slider.noUiSlider.get(), ['120.00', '150.00'], 'RTL set.');
 });
+
+QUnit.test("Limit option (non-linear)", function (assert) {
+
+    document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
+
+    var settings = {
+        start: [50, 310],
+        limit: 100,
+        range: {
+            'min': [0],
+            '20%': [160],
+            '40%': [240],
+            '60%': [280],
+            '80%': [300],
+            'max': [310]
+        }
+    };
+
+    var slider = document.getElementById('qunit-fixture').querySelector('.slider');
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['50.00', '150.00'], 'Limit applied on init.');
+
+    slider.noUiSlider.set([null, 600]);
+    assert.deepEqual(slider.noUiSlider.get(), ['50.00', '150.00'], 'Handle can\'t leave limit.');
+
+    slider.noUiSlider.set([150, 310]);
+    assert.deepEqual(slider.noUiSlider.get(), ['150.00', '250.00'], 'Multiple set limit.');
+
+    // Rebuild with new settings;
+    settings.direction = 'rtl';
+    slider.noUiSlider.destroy();
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['50.00', '150.00'], 'RTL correct value.');
+
+    slider.noUiSlider.set([120, 240]);
+    assert.deepEqual(slider.noUiSlider.get(), ['120.00', '220.00'], 'RTL set.');
+});
