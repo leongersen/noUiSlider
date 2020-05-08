125a7eab1e52e5f639548633afd47412e5dcd5a7
Macro-Jackson <frank@comphare.com>
2020-05-08 16:30:30 +0200

    Added test non-linear sliders
    Added test for margins, limits and padding of non-linear sliders

--- tests/slider_padding.js
+++ tests/slider_padding.js
@@ -35,6 +35,47 @@ QUnit.test("Padding option", function (assert) {
     assert.deepEqual(slider.noUiSlider.get(), ['3.00', '9.00'], 'RTL set.');
 });
 
+QUnit.test("Padding option (non-linear)", function (assert) {
+
+    document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
+
+    var settings = {
+        start: [0, 310],
+        padding: 20,
+        range: {
+            'min': [0],
+            '20%': [160],
+            '40%': [240],
+            '60%': [280],
+            '80%': [300],
+            'max': [310]
+        }
+    };
+
+    var slider = document.getElementById('qunit-fixture').querySelector('.slider');
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['20.00', '290.00'], 'Padding applied on init.');
+
+    slider.noUiSlider.set([40, 310]);
+    assert.deepEqual(slider.noUiSlider.get(), ['40.00', '290.00'], 'Handle can\'t pass padding.');
+
+    slider.noUiSlider.set([0, 270]);
+    assert.deepEqual(slider.noUiSlider.get(), ['20.00', '270.00'], 'Multiple set outside of padding');
+
+    // Rebuild with new settings;
+    settings.direction = 'rtl';
+    slider.noUiSlider.destroy();
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['20.00', '290.00'], 'RTL on init.');
+
+    slider.noUiSlider.set([20, 285]);
+    assert.deepEqual(slider.noUiSlider.get(), ['20.00', '285.00'], 'RTL set.');
+});
+
 QUnit.test("Padding option", function (assert) {
 
     document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
@@ -65,6 +106,40 @@ QUnit.test("Padding option", function (assert) {
 
 });
 
+QUnit.test("Padding option (non-linear)", function (assert) {
+
+    document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
+
+    var settings = {
+        start: [0, 310],
+        padding: [20, 5],
+        range: {
+            'min': [0],
+            '20%': [160],
+            '40%': [240],
+            '60%': [280],
+            '80%': [300],
+            'max': [310]
+        }
+    };
+
+    var slider = document.getElementById('qunit-fixture').querySelector('.slider');
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['20.00', '305.00'], 'Different paddings applied');
+
+    // =============
+
+    slider.noUiSlider.destroy();
+
+    settings.padding = [0, 10];
+
+    noUiSlider.create(slider, settings);
+    assert.deepEqual(slider.noUiSlider.get(), ['0.00', '300.00'], 'One of the padding values is 0');
+
+});
+
 QUnit.test("Padding values <= 100%", function (assert) {
 
     document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
@@ -83,5 +158,31 @@ QUnit.test("Padding values <= 100%", function (assert) {
     noUiSlider.create(slider, settings);
 
     assert.deepEqual(slider.noUiSlider.get(), ['30.00', '30.00'], 'Slider values can only be 30');
+
+});
+
+
+QUnit.test("Padding values <= 100% (non-linear)", function (assert) {
+
+    document.getElementById('qunit-fixture').innerHTML = '<div class="slider"></div>';
+
+    var settings = {
+        start: [0, 310],
+        padding: [155, 155],
+        range: {
+            'min': [0],
+            '20%': [160],
+            '40%': [240],
+            '60%': [280],
+            '80%': [300],
+            'max': [310]
+        }
+    };
+
+    var slider = document.getElementById('qunit-fixture').querySelector('.slider');
+
+    noUiSlider.create(slider, settings);
+
+    assert.deepEqual(slider.noUiSlider.get(), ['155.00', '155.00'], 'Slider values can only be 155');
 
 });
